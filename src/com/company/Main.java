cd1e640a4e10d921493fd4cef165d3a2ce71154e
Jade <56938630+jadekang0611@users.noreply.github.com>
2022-02-24 17:09:58 -0800

    cleaned up

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -9,39 +9,19 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         int minPrincipal = 1_000;
         int maxPrincipal = 1_000_000;
-        int principal = 0;
-        float interest = 0;
-        byte period = 0;
+        int principal;
+        float interest;
+        byte period;
         float minInterest = 0;
         float maxInterest = 30;
         final DecimalFormat df = new DecimalFormat("'$'0.00");
-        double mortgage = 0;
-        double balance = 0;
-//            while (true) {
-//                System.out.print("Principal ($1K - $1M): ");
-//                principal = scanner.nextInt();
-//                if (principal >= minPrincipal && principal <= maxPrincipal)
-//                    break;
-//                System.out.print("Enter a value between $1,000 and $1,000,000 ");
-//            }
+        double mortgage;
+        double balance;
+
         principal = (int) readNumber("Principal: ", minPrincipal, maxPrincipal);
         interest = (float)readNumber("Interest: ", minInterest, maxInterest);
-//            while (true) {
-//                System.out.print("Annual Interest Rate: ");
-//                interest = scanner.nextFloat();
-//                if (interest >= minInterest && interest <= maxInterest)
-//                    break;
-//                System.out.println("Enter a value greater than 0 and less than 30");
-//            }
         period = (byte)readNumber("Period: ", 0, 30);
-//            while (true) {
-//                System.out.print("Period (Years): ");
-//                period = scanner.nextByte();
-//                if (period >= 0 && period <= 30)
-//                    break;
-//                System.out.println("Enter a value greater than 0 and less than 30");
-//            }
-            mortgage = calculateMortgage(principal, interest, period);
+        mortgage = calculateMortgage(principal, interest, period);
             String mortgageFormatted = df.format(mortgage);
             System.out.println("Mortgage: " + mortgageFormatted);
             System.out.println();
@@ -51,7 +31,6 @@ public class Main {
                 balance = calculatePaymentSchedule(principal, interest, period, numberOfMonth);
                 String balanceFormatted = df.format(balance);
                 System.out.println(balanceFormatted);
-
             }
     }
 
@@ -69,7 +48,6 @@ public class Main {
     }
 
     public static double calculateMortgage(int principal, float interest, byte period) {
-//
         float monthlyInt = interest/12/100;
         int monthlyPd = (period * 12);
         float factor = (float)Math.pow(1+monthlyInt, monthlyPd);

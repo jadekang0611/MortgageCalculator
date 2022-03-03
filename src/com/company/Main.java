e2153b3106f86e5f047e4534e43cc05effc19f72
Jade <56938630+jadekang0611@users.noreply.github.com>
2022-03-02 16:10:06 -0800

    Refactored the code by removing unnecessary variable assignments and fixed a minor bug for mortgage payment schedule method.

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -4,30 +4,27 @@ import java.text.DecimalFormat;
 import java.util.Scanner;
 
 public class Main {
-
+    final static byte MONTHS_IN_YEAR = 12;
+    final static byte PERCENT = 100;
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
         int minPrincipal = 1_000;
         int maxPrincipal = 1_000_000;
-        int principal;
-        float interest;
-        byte period;
         float minInterest = 0;
         float maxInterest = 30;
-        final DecimalFormat df = new DecimalFormat("'$'0.00");
         double mortgage;
         double balance;
+        final DecimalFormat df = new DecimalFormat("'$'0.00");
 
-        principal = (int) readNumber("Principal: ", minPrincipal, maxPrincipal);
-        interest = (float)readNumber("Interest: ", minInterest, maxInterest);
-        period = (byte)readNumber("Period: ", 0, 30);
+        int principal = (int) readNumber("Principal: ", minPrincipal, maxPrincipal);
+        float interest = (float)readNumber("Interest: ", minInterest, maxInterest);
+        byte period = (byte)readNumber("Period: ", 0, 30);
         mortgage = calculateMortgage(principal, interest, period);
             String mortgageFormatted = df.format(mortgage);
-            System.out.println("Mortgage: " + mortgageFormatted);
+            System.out.println("MORTGAGE: " + mortgageFormatted);
             System.out.println();
             System.out.println("PAYMENT SCHEDULE");
             System.out.println("------------------");
-            for (byte numberOfMonth = 1; numberOfMonth < period*12; numberOfMonth++) {
+            for (short numberOfMonth = 1; numberOfMonth <= period * MONTHS_IN_YEAR; numberOfMonth++) {
                 balance = calculatePaymentSchedule(principal, interest, period, numberOfMonth);
                 String balanceFormatted = df.format(balance);
                 System.out.println(balanceFormatted);
@@ -38,18 +35,18 @@ public class Main {
         Scanner scanner = new Scanner(System.in);
         double value;
         while (true){
-        System.out.print(prompt);
-        value = scanner.nextFloat();
-        if (value >= min && value <= max)
-            break;
-        System.out.println("Enter a value between " + min + "and " + max);
+            System.out.print(prompt);
+            value = scanner.nextFloat();
+            if (value >= min && value <= max)
+                break;
+            System.out.println("Enter a value between " + min + "and " + max);
         }
         return value;
     }
 
     public static double calculateMortgage(int principal, float interest, byte period) {
-        float monthlyInt = interest/12/100;
-        int monthlyPd = (period * 12);
+        float monthlyInt = interest / PERCENT / MONTHS_IN_YEAR;
+        int monthlyPd = (period * MONTHS_IN_YEAR);
         float factor = (float)Math.pow(1+monthlyInt, monthlyPd);
         double mortgageAmt = (principal * (monthlyInt*factor)) / (factor - 1);
         return mortgageAmt;
@@ -57,11 +54,17 @@ public class Main {
     }
 
 
-    public static double calculatePaymentSchedule(int principal, float interest, byte period, byte numberOfPaymentsMade) {
-        float monthlyInt = interest / 12 / 100;
-        int monthlyPd = (period * 12);
+    public static double calculatePaymentSchedule(
+            int principal,
+            float interest,
+            byte period,
+            short numberOfPaymentsMade
+    ) {
+        float monthlyInt = interest / PERCENT / MONTHS_IN_YEAR;
+        int monthlyPd = (period * MONTHS_IN_YEAR);
         float factor = (float) Math.pow(1 + monthlyInt, monthlyPd);
-        double balanceUpdated = principal * (factor - Math.pow(1 + monthlyInt, numberOfPaymentsMade)) / (factor - 1);
+        double balanceUpdated = principal * (factor - Math.pow(1 + monthlyInt, numberOfPaymentsMade))
+                / (factor - 1);
 
         return balanceUpdated;
     }
